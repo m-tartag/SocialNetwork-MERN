8b201fc367558d2acc3100a99686ee161f1c12ce
m-tartag <mattbrs51@gmail.com>
2019-05-17 23:10:48 -0400

    gravatar and bcrypt functionality working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 const User = require('../../models/User');
@@ -9,9 +11,8 @@ const User = require('../../models/User');
 // @access    Public
 router.post(
   '/',
-  // can add another param to post
-  // First:
-  // invoking validator / check middleware
+
+  // Express-Validator
   [
     check('name', 'Name is required')
       .not()
@@ -22,36 +23,55 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  // using async/await
+  // Use Async/Await here - Important
   async (req, res) => {
     const errors = validationResult(req);
-    // error catch
+    // Fancy Error Catch
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // this pulls what I want out of req.body
+    // Destructure {req.body}
     const { name, email, password } = req.body;
 
+    // NEED Try/Catch Using Async/Await
     try {
-      const user = await User.findOne({ email });
+      // See if user ALREADY exists
+      let user = await User.findOne({ email });
       if (user) {
         res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        console.log('xxxx', user);
       }
 
-      // See if user exists
+      // Gravatar middleware
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
-      // Get users gravatar
+      // This creates a new instance of the user.. it doesnt SAVE IT YET
+      // Must call User.save to save.. must ENCRYPT PASSWORD BEFORE SAVE TO DB..
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-      // Encrypt password
+      // --------------------------
+      // Encrypt password Middleware
+      //----------------------------
+      // Pretty much boilerplate -- Check Documentation
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // And Finally SAVE USER TO DB
+      // ***************************
+      await user.save();
 
       // Return jsonwebtoken
-
-      User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(data => res.json(data));
 
       res.send('User Route');
     } catch (err) {
